9af7164b17cb3418337eb18959c770adf762cbdd
HarryStevens <harryjosephstevens@gmail.com>
2014-04-01 11:29:22 -0400

    starting to add images with array and jquery

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 /**
- * @author
+ * @author Harry Stevens
  */
+
+//This is an array of all of our images (in order)
+var images = ["drain_0m.png", "drain_50m.png", "drain_100m.png", "drain_200m.png", "drain_500m.png", "drain_1km.png", "drain_2km.png", "drain_3km.png", "drain_5km.png", "drain_ed.png", "drain_nl.png"];
+
+var index = 0;
+
+$(document).ready(function() {
+	$(".image-holder").html(
+		"<img src='images/"+images[index]+"' />"
+		
+	);
+});
+
